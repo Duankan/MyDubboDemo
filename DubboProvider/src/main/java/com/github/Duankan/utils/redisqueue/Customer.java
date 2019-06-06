9fc5731cf18afeab09eb4794ab04895230bdc01c
duankang <duankang@kingtopware.com>
2019-06-06 19:07:57 +0800

    redis做消息队列

--- DubboProvider/src/main/java/com/github/Duankan/utils/redisqueue/Customer.java
+++ DubboProvider/src/main/java/com/github/Duankan/utils/redisqueue/Customer.java
@@ -1,11 +1,9 @@
 package com.github.Duankan.utils.redisqueue;
-
 /**
  * @author dankin
  * @date 2019-05-31
- * @class redis消息生产者
+ * @class redis消息消费者
  * @descrtion redis做消息队列
  */
-public class Producer {
-
+public class Customer {
 }
